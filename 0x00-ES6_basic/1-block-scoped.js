29689aa24368fa12e812ff4ae2736bd371a290ce
LadyDammy <oreoluwadammie@gmail.com>
2023-06-29 09:55:57 -0700

    Modify function to avoid an overwrite

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const newTask = true;
     const newTask2 = false;
+  }  
     return [newTask, newTask2];
-  }
-
+ 
+}
+{
   return [task, task2];
 }
